6ad3a0ecc22ac4e23dd3e04c84b8d3601f4c7a8a
Marco Enrico <mvalviar@gmail.com>
2025-03-09 20:10:34 +0800

    tooling: install pre-commit

---
+++ ui/eslint.config.js
@@ -0,0 +1,104 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+// import love from "eslint-config-love";
+import cypress from "eslint-plugin-cypress";
+import _import from "eslint-plugin-import";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import promise from "eslint-plugin-promise";
+import react from "eslint-plugin-react";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      "**/dist",
+      "**/node_modules",
+      "**/.prettierrc",
+      "**/cypress",
+      "**/cypress.config.ts",
+      "**/eslint.config.js",
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:cypress/recommended",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
+      "plugin:jsx-a11y/recommended",
+      "plugin:promise/recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+      "plugin:prettier/recommended",
+      "prettier",
+    ),
+  ),
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+      cypress: fixupPluginRules(cypress),
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
+      "jsx-a11y": fixupPluginRules(jsxA11Y),
+      promise: fixupPluginRules(promise),
+      react: fixupPluginRules(react),
+      "simple-import-sort": simpleImportSort,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...cypress.environments.globals.globals,
+      },
+
+      parser: tsParser,
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        node: true,
+        typescript: true,
+        typescript: {
+          alwaysTryTypes: true,
+          project: "./tsconfig.json",
+        },
+      },
+    },
+
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
